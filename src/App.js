fd1f99668213da3e69436d678f75c9e9539c974d
egs125 <egs125@gmail.com>
2019-09-14 19:23:32 +0900

    useInput

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component, useState } from 'react';
 import './App.css';
 
-function App() {
+const useInput = (initialValue) => {
+  const [ value, setValue ] = useState(initialValue);
+  const onChange = e => {
+    console.log(e.target);
+  }
+  return { value, onChange }
+};
+
+const App = () => {
+  const name = useInput("Mr."); 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello</h1>
+      <input placeholder="Name" {...name}/>
     </div>
-  );
+  )
 }
 
 export default App;
